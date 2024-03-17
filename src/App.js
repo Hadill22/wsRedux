fbfcc365b7fb21ad06200664031819d5f34321a9
hadil <miladihadil19@gmail.com>
2024-03-18 00:07:04 +0100

    add fichier

---
+++ src/App.js
@@ -0,0 +1,18 @@
+
+import './App.css';
+import AddTask from "./components/AddTask/AddTask";
+
+import TaskItems from "./components/TaskItems/TaskItems";
+import TaskList from "./components/TaskList/TaskList";
+
+function App() {
+
+  return (
+    <>
+     <AddTask />
+      <TaskList />
+    </>
+  );
+}
+
+export default App;
